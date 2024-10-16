aae332871130d8e2359543276240ce54d1653524
keyxcode <keyxcode@gmail.com>
2024-10-16 21:50:24 +0700

    Implement packet receive buffer

--- project2/streamer.py
+++ project2/streamer.py
@@ -21,25 +21,37 @@ class Streamer:
         self.dst_ip = dst_ip
         self.dst_port = dst_port
 
+        self.send_seq_num = 0
+
+        self.recv_buffer = dict()
+        self.expected_seq_num = 0
+
     def send(self, data_bytes: bytes) -> None:
         """Note that data_bytes can be larger than one packet."""
-        seq_num = 0
 
         for i in range(0, len(data_bytes), CHUNK_SIZE):
             # use struct to ensure the header created with seq_num always stays in HEADER_FORMAT
-            header = struct.pack(HEADER_FORMAT, seq_num)
+            header = struct.pack(HEADER_FORMAT, self.send_seq_num)
             full_packet = header + data_bytes[i : i + CHUNK_SIZE]
             self.socket.sendto(full_packet, (self.dst_ip, self.dst_port))
-            seq_num += 1
+            self.send_seq_num += 1
 
     def recv(self) -> bytes:
-        """Blocks (waits) if no data is ready to be read from the connection."""
-        # your code goes here!  The code below should be changed!
+        """Blocks (waits) until the expected sequence number is received.
+        Handles out-of-order packet reception using a receive buffer.
+        Returns the data corresponding to the expected sequence number."""
+        while self.expected_seq_num not in self.recv_buffer:
+            packet, addr = self.socket.recvfrom()
+            header_size = struct.calcsize(HEADER_FORMAT)
+            header = packet[:header_size]
+            data = packet[header_size:][:]
+            seq_num = struct.unpack(HEADER_FORMAT, header)[0]
+            self.recv_buffer[seq_num] = data
 
-        # this sample code just calls the recvfrom method on the LossySocket
-        data, addr = self.socket.recvfrom()
-        # For now, I'll just pass the full UDP payload to the app
-        return data
+        res = self.recv_buffer[self.expected_seq_num]
+        del self.recv_buffer[self.expected_seq_num]
+        self.expected_seq_num += 1
+        return res
 
     def close(self) -> None:
         """Cleans up. It should block (wait) until the Streamer is done with all
